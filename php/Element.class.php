9e6608d863cc0f1eb2c28bbfe3f982da4d832e09
Marty Esterman <FluencyGames@users.noreply.github.com>
2021-08-14 11:08:00 -0400

    Initial commit

---
+++ php/Element.class.php
@@ -0,0 +1,99 @@
+<?php
+class Element {
+
+    public static function Footer() {
+?>
+                <div class="kingster-footer-wrapper ">
+                    <div class="kingster-footer-container kingster-container clearfix">
+                        <div class="kingster-footer-column kingster-item-pdlr kingster-column-15">
+                            <div id="text-2" class="widget widget_text kingster-widget">
+                                <div class="textwidget">
+                                    <p><img src="upload/footer-logo.png" alt="" />
+                                        <br /> <span class="gdlr-core-space-shortcode" id="span_1dd7_10"></span>
+                                        <br /> Main Campus (K-6th)
+                                        <br /> 1480 Joyner Avenue 
+					<br /> Marietta, GA 30060
+					<br /> 
+					<br /> Upper School Campus (7-10th)
+					<br /> 1650 Old Concord Road
+					<br /> Marietta, GA 30060
+                                        <p><span id="span_1dd7_11">(770) 874-8885</span>
+                                        <br /> <span class="gdlr-core-space-shortcode" id="span_1dd7_12"></span>
+                                        <br /> <a id="a_1dd7_8" href="mailto:frontdesk@stonehavenschool.org">frontdesk@stonehavenschool.org</a></p>
+                                    <div class="gdlr-core-divider-item gdlr-core-divider-item-normal gdlr-core-left-align">
+                                        <div class="gdlr-core-divider-line gdlr-core-skin-divider" id="div_1dd7_111"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="kingster-footer-column kingster-item-pdlr kingster-column-15">
+                            <div id="gdlr-core-custom-menu-widget-2" class="widget widget_gdlr-core-custom-menu-widget kingster-widget">
+                                <h3 class="kingster-widget-title">Academics</h3><span class="clear"></span>
+                                <div class="menu-our-campus-container">
+                                    <ul id="menu-our-campus" class="gdlr-core-custom-menu-widget gdlr-core-menu-style-plain">
+                                        <li class="menu-item"><a href="academics/lower-philosophy.html">Academic Philosophy</a></li>
+                                        <li class="menu-item"><a href="academics/scope-sequence.html">Curriculum</a></li>
+                                        <li class="menu-item"><a href="about-us/faculty.html">Faculty & Staff</a></li>
+                                        <li class="menu-item"><a href="https://careers.smartrecruiters.com/ForrestJohnson/the-stonehaven-school">Employment</a></li>
+                                        <li class="menu-item"><a href="academics/fine-arts.html">Fine Arts</a></li>
+                                        <li class="menu-item"><a href="academics/nature-studies.html">Nature Studies</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="kingster-footer-column kingster-item-pdlr kingster-column-15">
+                            <div id="gdlr-core-custom-menu-widget-3" class="widget widget_gdlr-core-custom-menu-widget kingster-widget">
+                                <h3 class="kingster-widget-title">Admissions</h3><span class="clear"></span>
+                                <div class="menu-campus-life-container">
+                                    <ul id="menu-campus-life" class="gdlr-core-custom-menu-widget gdlr-core-menu-style-plain">
+                                        <li class="menu-item"><a href="admissions/admissions-process.html">Admissions Process</a></li>
+                                        <li class="menu-item"><a href="admissions/affordability.html">Affordability</a></li>
+                                        <li class="menu-item"><a href="admissions/tours-openhouses.html">Tours & Open Houses</a></li>
+                                        <li class="menu-item"><a href="https://stonehaven.almastart.com/">Apply Now</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="kingster-footer-column kingster-item-pdlr kingster-column-15">
+                            <div id="gdlr-core-custom-menu-widget-4" class="widget widget_gdlr-core-custom-menu-widget kingster-widget">
+                                <h3 class="kingster-widget-title">Students & Parents</h3><span class="clear"></span>
+                                <div class="menu-academics-container">
+                                    <ul id="menu-academics" class="gdlr-core-custom-menu-widget gdlr-core-menu-style-plain">
+                                        <li class="menu-item"><a href="student-life/life.html">Student Life</a></li>
+                                        <li class="menu-item"><a href="student-life/athletics.html">Athletics</a></li>
+                                        <li class="menu-item"><a href="student-life/house.html">House</a></li>
+                                        <li class="menu-item"><a href="parents/bloghaven.html">Bloghaven</a></li>
+                                        <li class="menu-item"><a href="parents/cultivate.html">Cultivate</a></li>
+                                        <li class="menu-item"><a href="https://stonehavenschool.square.site/" target="new">School Store</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="kingster-copyright-wrapper">
+                    <div class="kingster-copyright-container kingster-container clearfix">
+                        <div class="kingster-copyright-left kingster-item-pdlr">Copyright All Rights Reserved 2021, The Stonehaven School</div>
+                        <div class="kingster-copyright-right kingster-item-pdlr">
+                            <div class="gdlr-core-social-network-item gdlr-core-item-pdb  gdlr-core-none-align" id="div_1dd7_112">
+                                <a href="https://www.facebook.com/stonehavenschool" target="_blank" class="gdlr-core-social-network-icon" title="facebook">
+                                    <i class="fa fa-facebook" ></i>
+                                </a>
+                                <a href="https://vimeo.com/user39763469" target="_blank" class="gdlr-core-social-network-icon" title="vimeo">
+                                    <i class="fa fa-vimeo" ></i>
+                                </a>
+                                <a href="https://twitter.com/StonehavenEDU" target="_blank" class="gdlr-core-social-network-icon" title="twitter">
+                                    <i class="fa fa-twitter" ></i>
+                                </a>
+                                <a href="https://www.instagram.com/thestonehavenschool/" target="_blank" class="gdlr-core-social-network-icon" title="instagram">
+                                    <i class="fa fa-instagram" ></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+<?php
+    }
+}
+?>
